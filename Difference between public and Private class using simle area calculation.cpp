bf0b5d27ae42e8ada5261666c37dbd2d3cc31e19
Md. Surat-E- Mostafa <surat153@gmail.com>
2020-01-27 15:17:13 +0200

    Update Difference between public and Private class using simle area calculation.cpp

--- Difference between public and Private class using simle area calculation.cpp
+++ Difference between public and Private class using simle area calculation.cpp
@@ -35,3 +35,43 @@ int main()
 	}	
 	
 
+// In the second part, now create the private and public class and use the public function outside the class but not Private class!
+
+#include <iostream> 
+using namespace std; 
+  
+class Circle { 
+    // private data member 
+private: 
+    double radius; 
+  
+    // public member function 
+public: 
+    void compute_area(double r) 
+    { 
+        // member function can access private 
+        // data member radius 
+        radius = r; 
+  
+        double area = 3.14 * radius * radius; 
+  
+        cout << "Radius is: " << radius << endl; 
+        cout << "Area is: " << area; 
+    } 
+}; 
+  
+// main function 
+int main() 
+{ 
+    // creating object of the class 
+    Circle radi; 
+  
+    // trying to access private data member 
+    // directly outside the class 
+    radi.compute_area(7); 
+  
+    return 0; 
+}
+	
+
+
